ffc57837b74ee4a1e76de7ef7eefa21112edd56e
caleb byrne <calebmbyrne@gmail.com>
2025-12-08 12:42:31 -0800

    String challenge find the first unique character that isn't repeated in a string.  Using a hash to count and the ruby method each_char to iterate through the string.

--- Strings/first_unique_char.rb
+++ Strings/first_unique_char.rb
@@ -3,5 +3,16 @@
 # Example: "leetcode" => "l", "loveleetcode" => "v"
 
 def first_unique_char(s)
-  # Solution here
+  char_count = Hash.new(0)
+  s.each_char do |key|
+    char_count[key] += 1
+  end
+  puts char_count
+
+  s.each_char do |key|
+    return key if char_count[key] == 1
+  end
+  "no unique characters in the hash #{char_count}"
 end
+
+puts first_unique_char("this method will find the first character that is only represented once")

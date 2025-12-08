03dc4cbe87baf445f578e9fe7d7ab62d736e3a70
caleb byrne <calebmbyrne@gmail.com>
2025-12-08 15:18:50 -0800

    minimum repeating character using two hashes.  1 to count and 1 hash to index then using the .select to filter out the the indexed hash and select the .value.min of the selected filter

--- Strings/first_unique_char.rb
+++ Strings/first_unique_char.rb
@@ -4,15 +4,19 @@
 
 def first_unique_char(s)
   char_count = Hash.new(0)
-  s.each_char do |key|
-    char_count[key] += 1
-  end
-  puts char_count
+  char_index = Hash.new(-1)
 
-  s.each_char do |key|
-    return key if char_count[key] == 1
+  # First pass: build both hashes
+  s.each_char.with_index do |char, index|
+    char_count[char] += 1
+    char_index[char] = index if char_count[char] == 1
   end
-  "no unique characters in the hash #{char_count}"
+
+  # After the loop, find the minimum index among chars with count=1
+  min_index = char_index.select { |char, _| char_count[char] == 1 }.values.min
+
+  # Return the character at that index, or nil if no unique char exists
+  min_index == -1 ? nil : s[min_index]
 end
 
 puts first_unique_char("this method will find the first character that is only represented once")

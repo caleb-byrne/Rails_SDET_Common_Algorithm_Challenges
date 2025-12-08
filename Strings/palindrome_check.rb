43f1a2fd8517e17b38df27b5bde92c8f440fcfbc
caleb byrne <calebmbyrne@gmail.com>
2025-12-08 15:37:10 -0800

    solved the palidrome check for strings using .downcase and .reverse.downcase then comparing each variable if forward == reverse return true.

--- Strings/palindrome_check.rb
+++ Strings/palindrome_check.rb
@@ -2,6 +2,15 @@
 # Determine if a string is a palindrome (reads the same forwards and backwards)
 # Example: "racecar" => true, "hello" => false
 
-def is_palindrome?(s)
-  # Solution here
+def palindrome_check(s)
+  forward = s.downcase
+  reverse = s.reverse.downcase
+ 
+  if forward == reverse
+    puts "#{forward} == #{reverse} \nThis is a palindrome"
+  else
+    puts "#{forward} != #{reverse} \nThis is not a palindrome"
+  end
 end
+
+palindrome_check("Racecar")

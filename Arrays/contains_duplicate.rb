55b68b7e1d9493eaedf4aebf28eba60faf6093a5
caleb byrne <calebmbyrne@gmail.com>
2025-12-09 15:31:25 -0800

    Using Set to check for quick look up of a value to see if it exists in the Set seen then adds it through the seen.add(num) and then returns true.

--- Arrays/contains_duplicate.rb
+++ Arrays/contains_duplicate.rb
@@ -1,7 +1,18 @@
+require 'set'
 # Challenge: Contains Duplicate
 # Return true if array contains any duplicate values
 # Example: [1, 2, 3, 1] => true
 
 def contains_duplicate?(nums)
-  # Solution here
+  seen = Set.new
+
+  nums.each do |num|
+    return true if seen.include?(num)
+    seen.add(num)
+  end
+
+  "False array does not contain duplicates"
 end
+
+puts contains_duplicate?([1, 2, 3, 1])
+puts contains_duplicate?([1, 2, 3, 5, 6])

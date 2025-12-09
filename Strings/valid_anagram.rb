63639bd729eb16283397ce2cf7d79b9a25766279
caleb byrne <calebmbyrne@gmail.com>
2025-12-09 11:30:45 -0800

    Uses ruby's .char method for strings to sort the string into individual characters and .sort to order them alphabetically then compares the two strings to see if they're equal.

--- Strings/valid_anagram.rb
+++ Strings/valid_anagram.rb
@@ -3,5 +3,12 @@
 # Example: "listen" and "silent" => true
 
 def is_anagram?(s, t)
-  # Solution here
+  if s.length != t.length
+    return "false: #{s} and #{t} aren't anagrams"
+  else
+    return "true: #{s} and #{t} are anagrams" if s.chars.sort == t.chars.sort
+    return "false: #{s} and #{t} aren't anagrams"
+  end
 end
+
+puts is_anagram?("listen", "silint")

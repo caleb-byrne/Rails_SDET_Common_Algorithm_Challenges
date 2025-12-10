3d245b7aeab6481feb44f6314490c597b6bebcbf
caleb byrne <calebmbyrne@gmail.com>
2025-12-09 16:25:49 -0800

    used the sort method to organize the numbers and since their from 0-n the array has to start with 0 so I can do .each_with_index on the array and then comapre the value to the index and if they don't match I return that index and it will be the missing number.

--- Arrays/find_missing_number.rb
+++ Arrays/find_missing_number.rb
@@ -3,5 +3,15 @@
 # Example: [3, 0, 1] => 2
 
 def find_missing_number(nums)
-  # Solution here
+  a = nums.sort
+  
+  a.each_with_index do |num, index|
+    return index if num != index
+  end
+
+  a.length
+  "no missing number"
 end
+
+puts find_missing_number([3, 0, 1])
+puts find_missing_number([ 0, 1, 2, 3, 4, 5])

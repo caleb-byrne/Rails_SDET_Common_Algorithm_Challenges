5e9624d3f4b00035b828787eea04f36c30403a3f
caleb byrne <calebmbyrne@gmail.com>
2025-12-10 09:58:34 -0800

    This is the simplest way.  Sorting the array and multiplying the last two numbers and then multiplying the first two and taking the max of whichever is biggest with the .max ruby method. That way if you have 2 negative numbers that are bigger than the to largest positive number product you can still get the corrrect answer.

--- Arrays/max_product.rb
+++ Arrays/max_product.rb
@@ -1,7 +1,12 @@
 # Challenge: Max Product of Two Elements
 # Return the maximum product of any two numbers in the array
-# Example: [2, 3, -2, 4] => 24
+# Example: [2, 3, -2, 4] => 12
 
 def max_product(nums)
-  # Solution here
+  ordered = nums.sort
+  [ordered[-1] * ordered[-2], ordered[0] * ordered[1]].max
 end
+
+
+puts max_product([2, 3, -2, 4])
+puts max_product([-1, -3, -4, 2, 0, -5])

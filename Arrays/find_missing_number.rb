8a231af3c3b1c9127f5dd7ca59622499acb53bd1
caleb byrne <calebmbyrne@gmail.com>
2025-12-09 16:46:01 -0800

    Added a math formula to multiple  consecutive numbers Plus one more and then subtract the expected sum from the actual sum of the array.

--- Arrays/find_missing_number.rb
+++ Arrays/find_missing_number.rb
@@ -3,15 +3,12 @@
 # Example: [3, 0, 1] => 2
 
 def find_missing_number(nums)
-  a = nums.sort
+  n = nums.length
+  expected_sum = n * (n + 1) / 2
+  actual_sum = nums.sum
   
-  a.each_with_index do |num, index|
-    return index if num != index
-  end
-
-  a.length
-  "no missing number"
+  expected_sum - actual_sum
 end
 
 puts find_missing_number([3, 0, 1])
-puts find_missing_number([ 0, 1, 2, 3, 4, 5])
+puts find_missing_number([0, 1, 2, 3, 4, 5])
